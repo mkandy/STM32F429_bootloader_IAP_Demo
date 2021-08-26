1e551fce1977c84297bcb1d38cda3f1f119c099a
mkandy <mkandy44@gmail.com>
2021-08-26 20:14:44 +0800

    addIAP

--- cyclone_boot/iap/drivers/stm32f4xx_flash_driver.h
+++ cyclone_boot/iap/drivers/stm32f4xx_flash_driver.h
@@ -38,7 +38,7 @@
 #include "error.h"
 
 //Flash sectors number
-#define FLASH_SECTORS_NUMBER 24
+#define FLASH_SECTORS_NUMBER 16
 
 //Flash Sector start address list (from STM32F7 datasheet)
 #define FLASH_SECTOR_0_ADDR   0x08000000 //Base address of Sector 0,  End address at 0x08003FFF, 16 Kbytes   | 16 Kbytes
@@ -49,22 +49,15 @@
 #define FLASH_SECTOR_5_ADDR   0x08020000 //Base address of Sector 5,  End address at 0x0803FFFF, 128 Kbytes  | 256 Kbytes
 #define FLASH_SECTOR_6_ADDR   0x08040000 //Base address of Sector 6,  End address at 0x0805FFFF, 128 Kbytes  | 384 Kbytes
 #define FLASH_SECTOR_7_ADDR   0x08060000 //Base address of Sector 7,  End address at 0x0807FFFF, 128 Kbytes  | 512 Kbytes
-#define FLASH_SECTOR_8_ADDR   0x08080000 //Base address of Sector 8,  End address at 0x0809FFFF, 128 Kbytes  | 640 Kbytes
-#define FLASH_SECTOR_9_ADDR   0x080A0000 //Base address of Sector 9,  End address at 0x080BFFFF, 128 Kbytes  | 768 Kbytes
-#define FLASH_SECTOR_10_ADDR  0x080C0000 //Base address of Sector 10, End address at 0x080DFFFF, 128 Kbytes  | 896 Kbytes
-#define FLASH_SECTOR_11_ADDR  0x080E0000 //Base address of Sector 11, End address at 0x080FFFFF, 128 Kbytes  | 1024 Kbytes
-#define FLASH_SECTOR_12_ADDR  0x08100000 //Base address of Sector 12, End address at 0x08103FFF, 16 Kbytes   | 16 Kbytes
-#define FLASH_SECTOR_13_ADDR  0x08104000 //Base address of Sector 13, End address at 0x08107FFF, 16 Kbytes   | 32 Kbytes
-#define FLASH_SECTOR_14_ADDR  0x08108000 //Base address of Sector 14, End address at 0x0810BFFF, 16 Kbytes   | 48 Kbytes
-#define FLASH_SECTOR_15_ADDR  0x0810C000 //Base address of Sector 15, End address at 0x0810FFFF, 16 Kbytes   | 64 Kbytes
-#define FLASH_SECTOR_16_ADDR  0x08110000 //Base address of Sector 16, End address at 0x0811FFFF, 64 Kbytes   | 128 Kbytes
-#define FLASH_SECTOR_17_ADDR  0x08120000 //Base address of Sector 17, End address at 0x0813FFFF, 128 Kbytes  | 256 Kbytes
-#define FLASH_SECTOR_18_ADDR  0x08140000 //Base address of Sector 18, End address at 0x0815FFFF, 128 Kbytes  | 384 Kbytes
-#define FLASH_SECTOR_19_ADDR  0x08160000 //Base address of Sector 19, End address at 0x0817FFFF, 128 Kbytes  | 512 Kbytes
-#define FLASH_SECTOR_20_ADDR  0x08180000 //Base address of Sector 20, End address at 0x0819FFFF, 128 Kbytes  | 640 Kbytes
-#define FLASH_SECTOR_21_ADDR  0x081A0000 //Base address of Sector 21, End address at 0x081BFFFF, 128 Kbytes  | 768 Kbytes
-#define FLASH_SECTOR_22_ADDR  0x081C0000 //Base address of Sector 22, End address at 0x081DFFFF, 128 Kbytes  | 896 Kbytes
-#define FLASH_SECTOR_23_ADDR  0x081E0000 //Base address of Sector 23, End address at 0x081FFFFF, 128 Kbytes  | 1024 Kbytes
+#define FLASH_SECTOR_12_ADDR   0x08080000 //Base address of Sector 8,  End address at 0x0809FFFF, 128 Kbytes  | 640 Kbytes
+#define FLASH_SECTOR_13_ADDR   0x08084000 //Base address of Sector 9,  End address at 0x080BFFFF, 128 Kbytes  | 768 Kbytes
+#define FLASH_SECTOR_14_ADDR  0x08088000 //Base address of Sector 10, End address at 0x080DFFFF, 128 Kbytes  | 896 Kbytes
+#define FLASH_SECTOR_15_ADDR  0x0808C000 //Base address of Sector 11, End address at 0x080FFFFF, 128 Kbytes  | 1024 Kbytes
+#define FLASH_SECTOR_16_ADDR  0x08090000 //Base address of Sector 12, End address at 0x08103FFF, 16 Kbytes   | 16 Kbytes
+#define FLASH_SECTOR_17_ADDR  0x080A0000 //Base address of Sector 13, End address at 0x08107FFF, 16 Kbytes   | 32 Kbytes
+#define FLASH_SECTOR_18_ADDR  0x080C0000 //Base address of Sector 14, End address at 0x0810BFFF, 16 Kbytes   | 48 Kbytes
+#define FLASH_SECTOR_19_ADDR  0x080E0000 //Base address of Sector 15, End address at 0x0810FFFF, 16 Kbytes   | 64 Kbytes
+
 
 //Flash Sector size
 #define FLASH_SECTOR_SIZE_16KB   0x4000
